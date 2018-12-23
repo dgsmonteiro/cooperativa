be1b738e9fc4c277582d2d5d8e413b9e76ae377f
Douglas Monteiro <douglasmonteiro@icloud.com>
2018-12-23 19:26:43 -0200

    update

---
+++ src/app/services/paciente.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import {User} from '../models/User';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PacienteService {
+
+  constructor(private http: HttpClient) { }
+
+  listar() {
+    return this.http.get<any>(`${environment.apiUrl}/pacientes`)
+        .pipe(map(pacientes => {
+            // login successful if there's a jwt token in the response
+            if (pacientes) {
+                console.log(JSON.stringify(pacientes));
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+            }
+
+            return pacientes;
+        }));
+}
+
+}
